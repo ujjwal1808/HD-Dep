381e2e2d667d3b42ea040ab707007cfe2d79ccc7
Ujjwal Maurya <88647832+ujjwal1808@users.noreply.github.com>
2025-05-11 15:18:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.get("/getallposts", async (req, res) => {
 });
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
